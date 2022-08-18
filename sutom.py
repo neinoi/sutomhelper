2e2516caac463d598f83d12db01221fa8e10f9d9
Julien Bellion <julien.bellion@free.fr>
2022-08-18 10:09:59 -0400

    Comments cleanup

--- sutom.py
+++ sutom.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import argparse
 import random
 
